508492ea0776efdab272758a0bf306a231cf07f5
indratdev <indrat.dev@gmail.com>
2022-08-10 17:34:14 +0700

    updated

--- lib/screens/ewatermark.dart
+++ lib/screens/ewatermark.dart
@@ -20,7 +20,11 @@ class _EWatermarkState extends State<EWatermark> {
   File? _watermarkImage;
   File? _watermarkedImage;
   File? imageFile;
+  var _image;
+  var imgBytes;
   final picker = ImagePicker();
+  var buttonsRowDirection = 1; //ROW DIRECTION
+  var buttonsColDirection = 2; //COLOUMN DIRECTION
 
   Future getOriginalImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
@@ -82,6 +86,133 @@ class _EWatermarkState extends State<EWatermark> {
     }
   }
 
+  pickImage() {
+    showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                side: const BorderSide(
+                  color: Colors.black,
+                ),
+              ),
+              insetPadding: const EdgeInsets.all(8),
+              elevation: 10,
+              titlePadding: const EdgeInsets.all(0.0),
+              title: Container(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 10, 0),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            alignment: FractionalOffset.topRight,
+                            child: GestureDetector(
+                              child: const Icon(
+                                Icons.clear,
+                                color: Colors.red,
+                              ),
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.warning_amber_sharp,
+                              size: 48,
+                            ),
+                            SizedBox(
+                              height: 15,
+                            ),
+                            Text(
+                              "Alert with Close Button",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w500,
+                                  fontStyle: FontStyle.normal),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Your Subscription Plan Expiered",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              contentPadding: const EdgeInsets.all(8),
+              content: buttonsRowDirection == 1
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width / 4,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                                pickImageFromGallery();
+                              },
+                              child: const Text("Galeri"),
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width / 4,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // isi disini
+                              },
+                              child: const Text("Kamera"),
+                            ),
+                          ),
+                        ])
+                  : Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: //_getColButtons(context),
+                          <Widget>[Text("Continue"), Text("Cancel")]));
+        });
+  }
+
+  pickImageFromGallery() async {
+    XFile? image = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (image != null) {
+      _image = image;
+      var t = await image.readAsBytes();
+      imgBytes = Uint8List.fromList(t);
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -92,6 +223,38 @@ class _EWatermarkState extends State<EWatermark> {
         child: Center(
           child: Column(
             children: <Widget>[
+              GestureDetector(
+                onTap: pickImage,
+                child: Container(
+                  margin: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  width: 600,
+                  height: 250,
+                  child: _image == null
+                      ? Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.add_a_photo),
+                                Text(" / "),
+                                Icon(Icons.browse_gallery_sharp),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text('Click here to choose image')
+                          ],
+                        )
+                      : Image.memory(imgBytes,
+                          width: 600, height: 200, fit: BoxFit.fitHeight),
+                ),
+              ),
               //<--------------- select original image ---------------->
               _originalImage == null
                   ? ElevatedButton(
